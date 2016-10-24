97d30a000a34ce59b1b0d800c7b92f5b5ca05e0d
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-10-23 19:09:12 -0700

    yow

--- main.py
+++ main.py
@@ -48,10 +48,11 @@ def main(host=None, master_mode=False):
     # for p in range(10):
     asyncio.ensure_future(led_controller(global_state, 19))
     asyncio.ensure_future(servo_controller(global_state))
-    asyncio.ensure_future(music_play(global_state))
 
     if master_mode:
         asyncio.ensure_future(timerrr(loop))
+
+    musicfinished_future = loop.run_in_executor(None, music_play, global_state)
 
     try:
         loop.run_forever()
--- musicplayer.py
+++ musicplayer.py
@@ -1,4 +1,34 @@
-import asyncio
+import time
+import pyaudio
+import wave
 
-async def music_play(filename):
-    print("I'm a wee MUSIC PLAYER")
+CHUNK = 1024
+FILE = "media/lonelyjack.wav"
+
+def music_play(filename):
+    # open the file for reading.
+    wf = wave.open(FILE, 'rb')
+    
+    # create an audio object
+    p = pyaudio.PyAudio()
+    
+    # open stream based on the wave object which has been input.
+    stream = p.open(format =
+                    p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+    
+    while True:
+         # read data (based on the CHUNK size)
+         data = wf.readframes(CHUNK)
+         
+         # play stream (looping from beginning of file to the end)
+         while data != '':
+             # writing to the stream is what *actually* plays the sound.
+             stream.write(data)
+             data = wf.readframes(CHUNK)
+    
+    # cleanup stuff.
+    stream.close()
+    p.terminate()
